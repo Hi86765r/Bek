2799fc088dd8f82253fdaa2acae5e002664fe32e
Hi86765r <128286531+Hi86765r@users.noreply.github.com>
2024-06-16 11:32:11 +0700

    Update XesterZ Hub V2.lua

--- XesterZ Hub V2.lua
+++ XesterZ Hub V2.lua
@@ -1,2 +1,2 @@
 --[[ We Are Making A New Version For XesterZ Hub So I Will Close The Older Version For XesterZ Hub Now. ]]--
-game:GetService("Players").LocalPlayer:Kick("Outdated version! Please Wait For The New XesterZ Hub V3.")
+game:GetService("Players").LocalPlayer:Kick("Outdated version! Please Wait For The New Version For XesterZ Hub.")
